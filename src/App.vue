c5a1bc823c3dedf17321777034299f18c0401cf0
Elwin Huaman <elwinlhq@hotmail.com>
2018-02-04 23:12:32 +0100

    Borrow System with Firebase
    Form completed, fix obligatory fields of text and so on.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <template>
   <div id="app">
+    <div class="container">
+      <div class="row">
+        <div class="twelve columns">
+          <!-- <form> -->            
+            <h5 class="docs-header">{{device}}</h5>
+            <p>If you would like to borrow a device from the RIC, please fill out the form below. Once you return the device, write the date
+returned and feel free to check out another device. Please treat the device kindly, so that we can all enjoy them</p>
+            <div class="row">
+              <div class="six columns">
+                <label>Names</label>
+                <input class="u-full-width" type="text" v-model="name" placeholder="E.g. Elwin Huaman...">
+                <label>Date</label>                
+                <input class="u-full-width" type="date" v-model="date"/>
+                <label for="exampleRecipientInput">Reason for Borrow</label>
+                <select class="u-full-width" v-model="selected">
+                  <option disabled value="">Please select one</option>
+                  <option value="Borrow">I am Borrowing(Borrow)</option>
+                  <option value="Return">I am Returning(Return)</option>
+                  <option value="Request">I will Borrow(Request)</option>
+                </select>
+                <label for="exampleMessage">Comments</label>
+                <textarea class="u-full-width" v-model="comments" placeholder="Why I need the Device?…"></textarea>
+              </div>             
+              <div class="six columns">
+                <label>Your Location</label>            
+                <div id="myMap">
+                  <button @click="getLocation()">See my Location</button>
+                  <label >{{errorMessage}}</label>
+                </div>
+              </div>
+            </div>                        
+            <label class="example-send-yourself-copy">
+              <input type="checkbox" id="checkbox" v-model="checked">
+              <span class="label-body">I have read and agree to the Terms</span>
+            </label>
+            <input class="button-primary" @click="submitBorrow()" type="submit" value="Submit">
+          <!-- </form> -->
+        </div>
+      </div>
+    </div>    
     <div>
-      <label>Name:</label>
+      <label for="exampleEmailInput">Name:</label>
       <input type="text" v-model="name"/>
       <label>Last Name</label>
-      <input type="text" v-model="lastName"/>
-      <input type="text" v-model="currentLocation.lat"/>
+      <input type="text" v-model="date"/>
       <button @click="submitBorrow()">Borrow</button>
     </div>
     <div>
@@ -18,20 +57,23 @@
 </template>
 
 <script>
+import GoogleMapsLoader from 'google-maps';
+import './css/skeleton.css';
 import { namesRef } from './firebase';
 export default {
   data () {
     return {
-      currentLocation: {lat:0, lng:0},
-      name: 'Elwin',
-      lastName: 'Huaman',
-      lat2: navigator.geolocation.getCurrentPosition((position) => {
-          this.currentLocation = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          };
-        })
+      device: 'Camera',
+      name: '',
+      date: '',
+      selected: '',
+      checked: '',
+      comments: '',
+      errorMessage: '',
+      currentLocation: {lat:0, lng:0}
     }
+  },
+  mounted: function() {    
   },
 //FIREBASE CONNECTION
   firebase: {
@@ -41,67 +83,49 @@ export default {
   methods: {
     submitBorrow(){
       namesRef.push({name: this.name, lastName: this.lastName, edit: false})
+    },
+    getLocation(){
+      if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(this.showMap,this.showError);
+      } else {
+        this.errorMessage = "Geolocation is not supported by this browser";
+      }
+    },
+    showMap(position){
+      this.currentLocation = {lat: position.coords.latitude, lng:position.coords.longitude};      
+      var map = new google.maps.Map(document.getElementById('myMap'), {
+            center: this.currentLocation,
+            scrollwheel: false,
+            zoom: 15
+            });
+      var marker = new google.maps.Marker({position:this.currentLocation});
+      marker.setMap(map);
+    },
+    showError(error){
+      switch(error.code){
+        case error.PERMISSION_DENIED:
+          this.errorMessage = "User denied the request for Geolocation."
+          break;
+        case error.POSITION_UNAVAILABLE:
+          this.errorMessage = "Location information is unavailable"
+          break;
+        case error.TIMEOUT:
+          this.errorMessage = "The Request to get user location time out."
+          break;
+        case error.UNKNOWN_ERROR:
+          this.errorMessage = "An unknown error ocurred."
+          break;
+      }
     }
   }
 }
 </script>
-
+<style scoped>
+#myMap {
+    height:300px;
+    width: 100%;
+   }
+</style>
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-input[type=text], select {
-    width: 100%;
-    padding: 12px 20px;
-    margin: 8px 0;
-    display: inline-block;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    box-sizing: border-box;
-}
-
-input[type=submit] {
-    width: 100%;
-    background-color: #4CAF50;
-    color: white;
-    padding: 14px 20px;
-    margin: 8px 0;
-    border: none;
-    border-radius: 4px;
-    cursor: pointer;
-}
-
-input[type=submit]:hover {
-    background-color: #45a049;
-}
-
-div{
-    size: 20px;
-    border-radius: 5px;
-    background-color: #f2f2f2;
-    padding: 20px;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './firebase';
 import Vue from 'vue'
 import App from './App.vue'
 import VueFire from 'vuefire';
+import './css/skeleton.css'
 
 Vue.use(VueFire);
 
